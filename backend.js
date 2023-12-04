82f4351201a996290a46bb529dd011e5c8ecd51f
Timberland9x <choistewart9x@yahoo.com>
2023-12-04 11:49:20 -0800

    inputs and buttons

---
+++ backend.js
@@ -0,0 +1,11 @@
+//output button
+function clicked(){
+    console.log("testing clicked");
+
+    var num1 = document.getElementById("numInput").value;
+    var num2 = document.getElementById("numInput2").value;
+    const outputDiv = document.createElement("div");
+    outputDiv.innerHTML = "output" + (num1 + num2);
+    console.log("output: " + (parseFloat(num1) + parseFloat(num2)));
+
+}
