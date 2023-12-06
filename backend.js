0841585ae83bddc65f7b0a1625753683909b0bcb
Timberland9x <choistewart9x@yahoo.com>
2023-12-05 21:05:48 -0800

    changed css

--- backend.js
+++ backend.js
@@ -1,11 +1,17 @@
 //output button
+
+var a = new Vector([])
 function clicked(){
     console.log("Clicked Func");
 
     var num1 = document.getElementById("numInput").value;
     var num2 = document.getElementById("numInput2").value;
+
     const outputDiv = document.createElement("div");
-    outputDiv.innerHTML = "output" + (num1 + num2);
-    console.log("output: " + (parseFloat(num1) + parseFloat(num2)));
+    outputDiv.innerHTML = "output: " + (parseFloat(num1) + parseFloat(num2));
+    document.body.appendChild(outputDiv);
+
+    console.log(outputDiv);
+
 
 }
