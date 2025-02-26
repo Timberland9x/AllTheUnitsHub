376645abb334e464f9d922b9cb4ab395ea8ae58b
Timberland9x <choistewart9x@yahoo.com>
2025-02-26 17:35:34 -0500

    Backend of creating conversions measurementName

--- CreateConversion.js
+++ CreateConversion.js
@@ -1,3 +1,24 @@
+//need to make a one to many connection for map
+
+let measurementName = [[]];
+
+measurementName[0][0]= "corner"
+measurementName[0].push("mile")
+
+measurementName.push("mile")
+measurementName.push("feet")
+measurementName[0].push("feet")
+
+// measurementName[1][1]= "1";
+
+console.log("Test!");
+for(let i = 0;  i < measurementName.length; i ++){
+  for(let j = 0;  j < measurementName[0].length; j ++){
+
+    console.log(measurementName[i][0-measurementName[0].length])
+}
+}
+
 //Ex: Car, Length
 export class conversionNames {
   //Name of the conversion
@@ -28,3 +49,5 @@ export default class unit {
     this.unitName2 = unitName2;
   }
 }
+
+
